9fa6bdf7ffb5813ad961caece1a37b56cc9c9bda
Hack Reactor <hackreactor@Hacks-Mac-mini-3.local>
2013-11-25 21:43:11 -0800

    second version

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,28 +1,33 @@
 $(document).ready(function(){
   // Set username and room
   var userName, roomName;
-  
+  var roomOptions = {};
+
   var setUser = function(){
     userName = escapeHTML(prompt("What's your name, user?"));
   };
 
-  var setRoom = function(){
-    roomName = prompt("Choose a room");
-    if (roomName == "") {
-      roomName = "Default";
-    }
-  };
+  // var setRoom = function(){
+  //   roomName = prompt("Choose a room");
+  //   if (roomName == "") {
+  //     roomName = "Default";
+  //   }
+  // };
 
   var escapeHTML = function (text) {
     var replacements = {"<": "&lt;", ">": "&gt;", "'": "&#39;", '"': "&quot;"};
-    return text.replace(/[<>"']/g, function (character) {
-      return replacements[character];
-    });
+    if (text){
+      return text.replace(/[<>"']/g, function (character) {
+        return replacements[character];
+      });
+    }
   };
   
   var textSet = function(messageObj) {
     for (var i = 0; i < 11; i++) {
-      $('#chat').prepend("<div class='message'>" + escapeHTML(messageObj[i].username) + ": " + escapeHTML(messageObj[i].text) + "</div>");
+      if (messageObj[i].username.length < 100 && messageObj[i].text.length < 150){
+        $('#chat').prepend("<div class='message'>" + escapeHTML(messageObj[i].username) + ": " + escapeHTML(messageObj[i].text) + "</div>");
+      }
     }
   };
   
@@ -36,7 +41,7 @@ $(document).ready(function(){
 	  },
 	  error: function (data){
 		  console.log("error occurred");
-	  },
+	  }
   };
 
   var postRequest = {
@@ -53,11 +58,41 @@ $(document).ready(function(){
   	}
   };
 
+  var getRoomsRequest = {
+    url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+    type: 'GET',
+    success: function (data) {
+      console.log(data);
+      for (var a = 0; a < data.results.length; a++){
+        var room = data.results[a].roomname;
+        if (room) {
+          roomOptions[room] = room;
+        }
+      }
+      console.log(roomOptions);
+    },
+    error: function (data) {
+      console.log("error occurred");
+    }
+  };
+
 // Click Handlers
+$('#refreshRoomsButton').on('click', function (event) {
+  $.ajax(getRoomsRequest);
+  var mySelect = $('#roomlist');
+  $.each(Object.keys(roomOptions), function(val, text) {
+    mySelect.append($('<option></option>').val(val).html(text));
+  });
+});
+
+// Select box needs to get the text, not the values, but using the .text method adds each name onto the roomName var. 
+$('#roomlist').change(function (event){
+  roomName = $('#roomlist').val();
+  console.log(roomName);
+});
+
 $('#refreshButton').on('click', function (event) {
 	$.ajax(getRequest);
-  //Escaping
-
 	//TODO
 });
 
@@ -80,5 +115,4 @@ $('#sendButton').click(function (event) {
 
 });
   setUser();
-  setRoom();
 });
